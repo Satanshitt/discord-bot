cc2d2b854c6e135f84be090df98398b5b3b019a5
Satanshitt <63668794+Satanshitt@users.noreply.github.com>
2020-08-26 02:16:08 -0300

    update test.js

--- commands/Example/test.js
+++ commands/Example/test.js
@@ -3,7 +3,7 @@ const Discord = require(`discord.js`);
 module.exports = {
     name: `test`,
     category: `Example`,
-    description: `a`,
+    description: `Test command!`,
     timeout: 5000,
     usage: `test`, 
     run: async (client, msg, args) => {
